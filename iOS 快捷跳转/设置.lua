a2b86a67e63e32cc223a065293eb2e86eaf34b19
touchsprite <56334053+chudongjingling@users.noreply.github.com>
2020-06-03 19:01:22 +0800

    Create 设置.lua

---
+++ iOS 快捷跳转/设置.lua
@@ -0,0 +1,55 @@
+设置部分	从窗口小部件（Prefs:）启动	从应用启动（App-prefs:）
+设置最顶层	Prefs:	App-prefs:
+关于	Prefs:root=General&path=About	App-prefs:root=General&path=About
+辅助功能	Prefs:root=General&path=ACCESSIBILITY	App-prefs:root=General&path=ACCESSIBILITY
+帐号设定	Prefs:root=ACCOUNT_SETTINGS	App-prefs:root=ACCOUNT_SETTINGS
+飞行模式	Prefs:root=AIRPLANE_MODE	App-prefs:root=AIRPLANE_MODE
+自动锁定iOS <10	Prefs:root=General&path=AUTOLOCK	App-prefs:root=General&path=AUTOLOCK
+自动锁定iOS> 10	Prefs:root=DISPLAY&path=AUTOLOCK	App-prefs:root=DISPLAY&path=AUTOLOCK
+电池	Prefs:root=BATTERY_USAGE	App-prefs:root=BATTERY_USAGE
+蓝牙iOS <9	Prefs:root=General&path=Bluetooth	App-prefs:root=General&path=Bluetooth
+蓝牙iOS> 9	Prefs:root=Bluetooth	App-prefs:root=Bluetooth
+蜂窝电话	Prefs:root=MOBILE_DATA_SETTINGS_ID	App-prefs:root=MOBILE_DATA_SETTINGS_ID
+约会时间	Prefs:root=General&path=DATE_AND_TIME	App-prefs:root=General&path=DATE_AND_TIME
+字典	Prefs:root=General&path=DICTIONARY	App-prefs:root=General&path=DICTIONARY
+显示与亮度	Prefs:root=Brightness	App-prefs:root=DISPLAY
+请勿打扰	Prefs:root=General&path=DO_NOT_DISTURB	App-prefs:root=General&path=DO_NOT_DISTURB
+视频对话	Prefs:root=FACETIME	App-prefs:root=FACETIME
+一般	Prefs:root=General	App-prefs:root=General
+iCloud的	Prefs:root=CASTLE	App-prefs:root=CASTLE
+的iTunes	Prefs:root=MUSIC	App-prefs:root=MUSIC
+iTunes均衡器	Prefs:root=MUSIC&path=EQ	App-prefs:root=MUSIC&path=EQ
+iTunes音量	Prefs:root=MUSIC&path=VolumeLimit	App-prefs:root=MUSIC&path=VolumeLimit
+键盘	Prefs:root=General&path=Keyboard	App-prefs:root=General&path=Keyboard
+琴键	Prefs:root=General&path=Keyboard/KEYBOARDS	App-prefs:root=General&path=Keyboard/KEYBOARDS
+语言和地区	Prefs:root=General&path=INTERNATIONAL	App-prefs:root=General&path=INTERNATIONAL
+位置服务	Prefs:root=LOCATION_SERVICES	App-prefs:root=LOCATION_SERVICES
+音乐	Prefs:root=MUSIC	App-prefs:root=MUSIC
+网络iOS <10？	Prefs:root=General&path=Network	App-prefs:root=General&path=Network
+耐克iPod	Prefs:root=NIKE_PLUS_IPOD	App-prefs:root=NIKE_PLUS_IPOD
+笔记	Prefs:root=NOTES	App-prefs:root=NOTES
+通知事项	Prefs:root=NOTIFICATIONS_ID	App-prefs:root=NOTIFICATIONS_ID
+存折	Prefs:root=PASSBOOK	App-prefs:root=PASSBOOK
+密码	Prefs:root=SAFARI&path=Passwords	App-prefs:root=SAFARI&path=Passwords
+个人热点	Prefs:root=INTERNET_TETHERING	App-prefs:root=INTERNET_TETHERING
+电话	Prefs:root=Phone	App-prefs:root=Phone
+照片和相机	Prefs:root=Photos	App-prefs:root=Photos
+隐私	Prefs:root=Privacy	App-prefs:root=Privacy
+配置文件和设备管理	Prefs:root=General&path=ManagedConfigurationList	App-prefs:root=General&path=ManagedConfigurationList
+重启	Prefs:root=General&path=Reset	App-prefs:root=General&path=Reset
+铃声	Prefs:root=Sounds&path=Ringtone	App-prefs:root=Sounds&path=Ringtone
+苹果浏览器	Prefs:root=SAFARI	App-prefs:root=SAFARI
+Siri iOS <10吗？	Prefs:root=General&path=Assistant	App-prefs:root=General&path=Assistant
+Siri iOS> 10？	Prefs:root=SIRI	App-prefs:root=SIRI
+声音与触觉	Prefs:root=Sounds	App-prefs:root=Sounds
+软件更新	Prefs:root=General&path=SOFTWARE_UPDATE_LINK	App-prefs:root=General&path=SOFTWARE_UPDATE_LINK
+储存与备份	Prefs:root=CASTLE&path=STORAGE_AND_BACKUP	App-prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+商店	Prefs:root=STORE	App-prefs:root=STORE
+推特	Prefs:root=TWITTER	App-prefs:root=TWITTER
+用法	Prefs:root=General&path=USAGE	App-prefs:root=General&path=USAGE
+视频	Prefs:root=VIDEO	App-prefs:root=VIDEO
+VPN iOS <10吗？	Prefs:root=General&path=Network/VPN	App-prefs:root=General&path=Network/VPN
+VPN iOS> 10吗？	Prefs:root=VPN	App-prefs:root=VPN
+钱包和Apple Pay	shoebox://url-scheme	shoebox://url-scheme
+墙纸	Prefs:root=Wallpaper	App-prefs:root=Wallpaper
+无线上网	Prefs:root=WIFI	App-prefs:root=WIFI
