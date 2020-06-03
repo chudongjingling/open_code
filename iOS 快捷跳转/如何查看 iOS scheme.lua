1d5a31079f33a2a3ce1c0bdd69a80528312972bc
touchsprite <56334053+chudongjingling@users.noreply.github.com>
2020-06-03 19:04:30 +0800

    Create 如何查看 iOS scheme.lua

---
+++ iOS 快捷跳转/如何查看 iOS scheme.lua
@@ -0,0 +1,4 @@
+1. 找到 ipa 文件，重命名扩展名为 zip
+2. 解压缩
+3. 打开解压文件，在搜索框内输入Info.plist，找到Info.plist，如果搜索出来有多个Info.plist请选择文件较大的
+4. 用编辑器打开Info.plist，按ctr+f，在查找框内输入CFBundleURLSchemes，红色框出来的就是我们要找的url参数，如果有多个，也一并记录即可
