6f08b746a91dcfe8e772d45201098369a89650c7
touchsprite <56334053+chudongjingling@users.noreply.github.com>
2020-06-03 19:25:34 +0800

    Create mini2.lua

---
+++ 天天三国志-扫荡/mini2.lua
@@ -0,0 +1,356 @@
+t_Login = {
+	{680, 878, 0xfbb600},
+	{1362, 882, 0xfab700},
+	{1328, 728, 0x21c963},
+	{680, 1052, 0x21caba},
+	{678, 1234, 0x00b1f1},
+	{1354, 1320, 0x00b0f0},
+	action_1 = function() click(1012,1104) end,
+}
+
+t_Reg = {
+	{716, 568, 0x000000},
+	{656, 566, 0x000000},
+	{678, 1086, 0xfbb600},
+	{1362, 1168, 0xfbb600},
+	{1366, 1084, 0xfbb600},
+	action_1 = function() click(914, 682) end,
+	action_2 = function() click(874, 818) end,
+	action_3 = function() click(894, 954) end,
+	action_4 = function() click(1002,1136) end,
+}
+
+t_Gg = {
+	{946, 1322, 0xa13f20},
+	{1126, 1324, 0x9c3e20},
+	{944, 1387, 0xaf3213},
+	{1126, 1389, 0xa92f12},
+	action_1 = function() click(1028, 1356) end
+}
+
+t_Go = {
+	{740, 1170, 0xb14b29},
+	{1296, 1176, 0xa63f21},
+	{746, 1246, 0xb53514},
+	{1296, 1250, 0xb03112},
+	{1994, 1444, 0x962b0e},
+	action_1 = function() click(1010, 1218) end
+}
+
+t_Talk = {
+	{1868, 1400, 0xdeb74f},
+	{1930, 1400, 0xe4b951},
+	{1900, 1436, 0xe4c673},
+	{1900, 1418, 0xe2bc5c},
+	action_1 = function() click(1786, 1390) end
+}
+
+t_ct = {
+	{464, 1006, 0xa95312},
+	{806, 1000, 0xbe7d41},
+	{712, 998, 0xf6efce},
+}
+
+t_First = {
+	{930, 672, 0x81ca0d},
+	{914, 672, 0x81ca0d},
+	{2022, 914, 0xe72814},
+	action_1 = function() click(850, 754) end,
+}
+
+t_Two = {
+	{1024, 672, 0xea3216},
+	{1078, 674, 0xe72814},
+	{1114, 672, 0xea3216},
+	action_1 = function() click(1036, 758) end,
+}
+
+t_Three = {
+	{1039, 674, 0x7fc40c},
+	{1023, 671, 0x82ce0e},
+	{1280, 675, 0xe52314},
+	{1297, 675, 0xe52314},
+	action_1 = function() click(134, 1377) end,
+}
+
+t_Four = {
+	{216, 1286, 0xe53c30},
+	{214, 1305, 0xf5dea9},
+	{215, 1324, 0xe02121},
+	action_1 = function() click(1268,775) end,
+}
+
+t_Five= {
+	{820, 434, 0x7fc40c},
+	{820, 673, 0x7fc50c},
+	{1022, 675, 0x7fc40c},
+	{859, 912, 0x81ca0d},
+	action_1 = function() click(1658, 1412) end,
+}
+
+t_End = {
+	{889, 1325, 0x2c5455},
+	{1155, 1327, 0x2d5457},
+	{888, 1398, 0x166a64},
+	{1162, 1400, 0x15655d},
+	{1102, 1345, 0x2f6160},
+	action_1 = function() click(1016, 1364) end,
+}
+
+t_Name = {
+	{1310, 533, 0x2c5252},
+	{1473, 541, 0x2c5254},
+	{1304, 593, 0x14645e},
+	{1480, 596, 0x146159},
+	action_1 = function() click(1390, 570) end,
+}
+
+t_JiuGo = {
+	{590, 301, 0xfe3e22},
+	{467, 1007, 0x964303},
+	{803, 1005, 0xaf5a16},
+	action_1 = function() click(453, 542) end,
+}
+
+t_JiuGuan = {
+	{532, 1370, 0x603825},
+	{670, 1374, 0x5b3827},
+	{680, 1440, 0x651b0d},
+	action_1 = function() click(1550, 1408) end,
+}
+
+t_Chou = {
+	{1814, 282, 0x922605},
+	{1814, 208, 0x5a2d1d},
+	{1810, 254, 0xefded6},
+	{1470, 1122, 0x802410},
+	action_1 = function() click(552, 1094) end,
+}
+
+t_ChouOK = {
+	{1042, 698, 0xffdf51},
+	{1044, 728, 0xffcd31},
+	{432, 1338, 0x19cbff},
+	action_1 = function() click(1416, 1352) end,
+}
+
+t_Master = {
+	{1504, 70, 0xf0eeb7},
+	{1982, 72, 0xefecb1},
+	{1036, 70, 0xf0eeb5},
+	action_1 = function() click(1502, 1066) end, --征战
+	action_2 = function() click(448,574) end, --酒馆
+	action_3 = function() click(108,904) end, --开服礼包
+	action_4 = function() click(156,126) end, --头像
+	action_5 = function() click(1642,1408) end, --任务
+	action_6 = function() click(1904,636) end, --邮件
+}
+
+t_Map = {
+	{92, 1444, 0xfff78c},
+	{516, 1444, 0xfff791},
+	{1174, 1444, 0xfff793},
+	{1966, 354, 0x9d2705},
+	action_1 = function() click(226, 556) end,
+}
+
+t_FightGo = {
+	{420, 510, 0xfff2dc},
+	{1962, 360, 0xa82805},
+	{1660, 1082, 0x972810},
+	action_1 = function() click(1616, 1402) end,
+}
+
+t_Xuan = {
+	{196, 493, 0xfaf6f3},
+	{326, 485, 0xfefaee},
+	{706, 492, 0xfefaef},
+	{1105, 525, 0xfffffe},
+	action_1 = function() click(214, 730) end,
+	action_2 = function() click(464,724) end,
+}
+
+t_Start = {
+	{219, 782, 0xff721b},
+	{241, 808, 0xff4d00},
+	{279, 769, 0xff8c2b},
+	{504, 802, 0xff5000},
+	action_1 = function() click(1613, 1393) end,
+}
+
+t_FightIn = {
+	{1776, 66, 0xefd9d2},
+	{1804, 98, 0xf0decf},
+	{1940, 66, 0xf0dcd1},
+	{1986, 98, 0xefdcd2},
+	action_1 = function() click(1024, 946) end,
+}
+
+t_Fight_1 = {
+	{322, 362, 0x7fc40c},
+	{374, 360, 0x81ca0d},
+	{1986, 358, 0xea3717},
+	{2004, 840, 0xea3216},
+	action_1 = function() click(1020, 938) end,
+}
+
+t_Fight_2 = {
+	{44, 842, 0x7fc40c},
+	{80, 842, 0x7fc40c},
+	{98, 840, 0x81ca0d},
+	action_1 = function() click(1016, 946) end,
+}
+
+t_Fight_3 = {
+	{1727, 360, 0xea3216},
+	{1711, 837, 0xea3717},
+	{1060, 840, 0x81ca0d},
+	action_1 = function() click(59, 461) end,
+	action_2 = function() click(1266,464) end,
+}
+
+t_Fight_4 = {
+	{1100, 672, 0x81ca0d},
+	{1520, 676, 0xe32014},
+	{1580, 674, 0xe92b14},
+	{1914, 1402, 0x561700},
+	{1928, 1474, 0xbc3411},
+	action_1 = function() click(1022, 770) end,
+	action_2 = function() click(1490, 774) end,
+}
+
+t_Fight_5 = {
+	{996, 674, 0x7fc40c},
+	{514, 1154, 0x7fc40c},
+	{1120, 1150, 0xea3717},
+	action_1 = function() click(416, 1250) end,
+	action_2 = function() click(1027, 950) end,
+}
+
+t_Stop = {
+	{1486, 672, 0xea3216},
+	{1538, 674, 0xe72814},
+	{1596, 676, 0xe72714},
+	{1044, 674, 0x7fc40c},
+	action_1 = function() click(1902, 1402) end,
+}
+
+t_Win = {
+	{864, 228, 0xfeb70b},
+	{1200, 214, 0xfeb30a},
+	{852, 360, 0xf9e843},
+	{1096, 362, 0xfbe845},
+	{1190, 348, 0xfbe342},
+	action_1 = function() click(1022, 1342) end,
+}
+
+t_CloseMap = {
+	{88, 1444, 0xfff78f},
+	{518, 1442, 0xfff994},
+	{845, 1449, 0xfff78f},
+	{1056, 1410, 0xa71400},
+	{1174, 1448, 0xfff78f},
+	action_1 = function() click(1962, 322) end,
+}
+
+t_Gift = {
+	{1836, 324, 0x5a2d1c},
+	{696, 242, 0xffc71e},
+	{1836, 388, 0x902305},
+	{860, 292, 0xfdc91a},
+	action_1 = function() click(1716, 580) end,
+	action_2 = function() click(1830, 358) end, --关闭
+}
+
+t_GiftEnd = {
+	{1670, 538, 0xff0000},
+	{1781, 580, 0xff0000},
+	{1766, 624, 0xfe0000},
+	{1650, 584, 0xff0000},
+}
+
+t_RenWu = {
+	{310, 212, 0xb36e4b},
+	{1740, 178, 0x5a2d1c},
+	{1738, 256, 0xab2906},
+	{1738, 216, 0xefded6},
+	action_1 = function() click(1740, 214) end,
+}
+
+t_LinQu = {
+	{1420, 424, 0x2b5254},
+	{1418, 474, 0x145f5a},
+	{1414, 454, 0x104946},
+	action_1 = function() click(1508, 450) end,
+}
+
+t_lqOK = {
+	{918, 876, 0x2d5556},
+	{1148, 872, 0x2c5555},
+	{912, 938, 0x156863},
+	{1148, 940, 0x166b62},
+	action_1 = function() click(1042, 908) end,
+}
+
+t_Panl = {
+	{1502, 464, 0x317963},
+	{1360, 460, 0x406b59},
+	{622, 1094, 0x9d2910},
+	{1010, 1092, 0x1bafac},
+	{1528, 1090, 0x15675f},
+	action_1 = function() click(1012, 1062) end,
+}
+
+t_Set = {
+	{725, 684, 0xffa546},
+	{1377, 702, 0xffa247},
+	{1337, 880, 0x2b5252},
+	{1508, 938, 0x135e57},
+	action_1 = function() click(910,912) end,
+	action_2 = function() click(1417, 917) end,
+	action_3 = function() click(1684,370) end,
+}
+
+t_Mail = {
+	{318, 172, 0xbf8058},
+	{961, 122, 0xe8c99f},
+	{1078, 188, 0xecb78a},
+	{1097, 137, 0xd59b57},
+	{992, 192, 0xd5893e},
+	{1722, 210, 0x992605},
+	action_1 = function() click(1722, 174) end,
+}
+
+t_Mails = {
+	{1384, 330, 0x2b5254},
+	{1552, 328, 0x2c5254},
+	{1381, 388, 0x146660},
+	{1560, 388, 0x146159},
+	action_1 = function() click(1458, 362) end,
+}
+
+t_NoMail = {
+	{340, 134, 0xb1a47f},
+	{320, 1404, 0xc0a266},
+	{1698, 1514, 0xc59e6c},
+	{1706, 1292, 0xbd9b6b},
+	action_1 = function() click(1724,178) end,
+}
+
+t_GetMail = {
+	{854, 1340, 0x2d5656},
+	{1160, 1346, 0x2c5457},
+	{844, 1426, 0x176a65},
+	{1162, 1430, 0x156860},
+	action_1 = function() click(1012,1390) end,
+}
+
+t_Card = {
+	{430, 1340, 0x1dcdff},
+	{784, 1302, 0x5b3626},
+	{1054, 1394, 0x721c0e},
+	{1280, 1308, 0x2c5555},
+	{1542, 1382, 0x15685f},
+}
+
+
